0bb21f90b35b95a7a0df9d2b4912065c065b4add
Tasqu <topi.rasku@vtt.fi>
2023-10-12 09:23:03 +0300

    Change variation period default value and autoformatting

--- process_datastore.jl
+++ process_datastore.jl
@@ -22,7 +22,7 @@ if length(ARGS) < 2
     4. num_lids=Inf, number of `location_id` objects included in the resulting datastore (e.g. for testing)
     5. thermal_conductivity_weight=0.5, how thermal conductivity of the materials is sampled. Average by default.
     6. interior_node_depth=0.1, how deep the interior thermal node is positioned within the structure. Based on the calibrations performed in https://cris.vtt.fi/en/publications/sensitivity-of-a-simple-lumped-capacitance-building-thermal-model
-    7. variation_period=2225140, "period of variations" as defined in EN ISO 13786:2017 Annex C. Based on the calibrations performed in https://cris.vtt.fi/en/publications/sensitivity-of-a-simple-lumped-capacitance-building-thermal-model
+    7. variation_period=1209600, "period of variations" as defined in EN ISO 13786:2017 Annex C. Based on the calibrations performed in https://cris.vtt.fi/en/publications/sensitivity-of-a-simple-lumped-capacitance-building-thermal-model, equal to two weeks in seconds.
     """
 else
     # Process command line arguments
@@ -33,20 +33,20 @@ else
     num_lids = parse(Float64, get(kws, "num_lids", "Inf"))
     tcw = parse(Float64, get(kws, "thermal_conductivity_weight", "0.5"))
     ind = parse(Float64, get(kws, "interior_node_depth", "0.1"))
-    vp = parse(Float64, get(kws, "variation_period", "2225140"))
+    vp = parse(Float64, get(kws, "variation_period", "1209600"))
 
     # Open input datastore and run tests.
     @info "Opening input datastore at `$(url_in)`..."
     @time using_spinedb(url_in, m)
     @info "Running structural input data tests..."
-    @time run_structural_tests(; limit = Inf, mod = m)
+    @time run_structural_tests(; limit=Inf, mod=m)
     @info "Running statistical input data tests..."
-    @time run_statistical_tests(; limit = Inf, mod = m)
+    @time run_statistical_tests(; limit=Inf, mod=m)
 
     # Process stuff
     create_processed_statistics!(m, num_lids, tcw, ind, vp)
 
     # Import processed data into the datastore at `url_out`
-    import_processed_data(url_out; scramble_data = scramble_data)
+    import_processed_data(url_out; scramble_data=scramble_data)
     @info "Done"
 end
